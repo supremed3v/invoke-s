7b719a216cb3de8ce8443605316c422161bcdcfe
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2023-12-09 01:28:20 +0500

    customized login/signup/recover password pages including forms

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -6,12 +6,33 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import { RenderParams } from '../../_components/RenderParams'
+import Image from 'next/image'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={100} height={100} className={classes.logo} />
+        </Link>
+      </div>
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <Link href="/login" className={classes.backLink}>
+            <Image src="/assets/icons/arrow-left.svg" alt="back" width={24} height={24} />
+            <p>Back</p>
+          </Link>
+          <div className={classes.formTitle}>
+            <h3>Forgot Password</h3>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
